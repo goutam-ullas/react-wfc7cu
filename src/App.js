f6571d854a7203773a4c63b359e2e7046b1bba75
goutam-ullas <goutam.ullas@gmail.com>
2020-09-24 19:41:00 +0200

    1 file modified

--- src/App.js
+++ src/App.js
@@ -522,7 +522,7 @@ class Application extends React.Component {
         themeWidth: 0.375,
         themeDescWidth: "100%",
         themeDescBottom: 600,
-        themeVidLeft:0
+        themeVidLeft:100
       });
     } else {
       this.setState({
@@ -586,7 +586,7 @@ class Application extends React.Component {
         themeWidth: 0.375,
         themeDescWidth: "100%",
         themeDescBottom: 600,
-        themeVidLeft: 0
+        themeVidLeft: 100
       });
     } else {
       this.setState({
